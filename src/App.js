2ebce1c5e51558320a2794cdaffb3e913c256659
simon <wanwanzainaer@gmail.com>
2021-07-31 21:16:30 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hi there hasdfasdfi here
         </a>
       </header>
     </div>
